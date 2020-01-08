a5ee55e885beccd4d132fd15c27a5d0d9e95da33
VitoLeonhardt <witold.chojnowski@gmail.com>
2020-01-08 15:35:37 +0100

    Added some validation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Input, Button, Dropdown, Radio } from "semantic-ui-react";
 import logo from './logo.svg';
 import './App.css';
 import { DateInput } from 'semantic-ui-calendar-react';
+import moment from "moment";
 
 const countryOptions = [
   {
@@ -41,23 +42,52 @@ const nationalityOptions = [
 ];
 
 function App() {
+  const [name, setName] = useState(undefined);
+  const [surname, setSurname] = useState(undefined);
+  const [passport, setPassport] = useState(undefined);
   const [country, setCountry] = useState(undefined);
   const [gender, setGender] = useState(undefined);
   const [nationality, setNationality] = useState(undefined);
   const [dateOfBirth, setDateOfBirth] = useState(undefined);
   const [passExpirationDate, setPassExpirationDate] = useState(undefined);
+
+  const isValidName = (str) => {
+    return str && str.length > 0 && str[0] === str[0].toUpperCase();
+  }
+
+  const isValueSpecified = (val) => {
+    return !!val;
+  }
+
+  const isDoBValid = (date) => {
+    if(!moment(date).isValid()) return false;
+    const eighteenYearsAgo = moment().subtract(18, "years");
+    const hundredYearsAgo = moment().subtract(100, "years");
+    return moment(date).isBetween(hundredYearsAgo, eighteenYearsAgo);
+  }
+
+  const isPassExpDateValid = (date) => {
+    return isDoBValid(dateOfBirth) && moment(date).isAfter(dateOfBirth) && moment().isBefore(date);
+  };
+
+  const isFormValid = () => {
+    return isValidName(name) && isValidName(surname) && 
+    isValueSpecified(country) && isValueSpecified(gender) && isValueSpecified(nationality)
+    && isDoBValid(dateOfBirth) && isPassExpDateValid(passExpirationDate);
+  }
   return (
     <div>
-      <Input placeholder="Name"/>
-      <Input placeholder="Surname"/>
-      <Input placeholder="eg. 234234"/>
+      <Input placeholder="Name" value={name} onChange={(_, { value }) => setName(value)}/>
+      <Input placeholder="Surname" value={surname} onChange={(_, { value }) => setSurname(value)}/>
+      <Input placeholder="eg. 234234" value={passport} onChange={(_, { value }) => setPassport(value)}/>
       <Dropdown value={country} selection options={countryOptions} onChange={(_, { value }) => setCountry(value)}/>
       <Radio label="Male" checked={gender === "M"} onClick={() => setGender("M")}/>
       <Radio label="Female" checked={gender === "F"} onClick={() => setGender("F")}/>
       <Radio label="Not specified" checked={gender === "N"} onClick={() => setGender("N")}/>
       <Dropdown value={nationality} selection options={nationalityOptions} onChange={(_, { value }) => setNationality(value)}/>
-      <DateInput value={dateOfBirth} onChange={(_, { value }) => setDateOfBirth(value)} placeholder="Date of Birth"/>
-      <DateInput value={passExpirationDate} onChange={(_, { value }) => setPassExpirationDate(value)} placeholder="Passport expiration date"/>
+      <DateInput dateFormat="YYYY-MM-DD" value={dateOfBirth} onChange={(_, { value }) => setDateOfBirth(value)} placeholder="Date of Birth"/>
+      <DateInput dateFormat="YYYY-MM-DD" value={passExpirationDate} onChange={(_, { value }) => setPassExpirationDate(value)} placeholder="Passport expiration date"/>
+      <Button disabled={!isFormValid()}>Submit</Button>
     </div>
   );
 }
