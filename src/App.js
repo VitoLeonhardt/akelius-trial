8bfb09eb5d9f2d373d3cda41ec59cf3820b41e00
VitoLeonhardt <witold.chojnowski@gmail.com>
2020-01-08 15:02:29 +0100

    Added a basic form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input, Button, Dropdown, Radio } from "semantic-ui-react";
 import logo from './logo.svg';
 import './App.css';
+import { DateInput } from 'semantic-ui-calendar-react';
+
+const countryOptions = [
+  {
+    key: 'Germany',
+    text: 'Germany',
+    value: 'Germany'
+  },
+  {
+    key: 'Poland',
+    text: 'Poland',
+    value: 'Poland'
+  },
+  {
+    key: 'UK',
+    text: 'UK',
+    value: 'UK'
+  }
+];
+
+const nationalityOptions = [
+  {
+    key: 'German',
+    text: 'German',
+    value: 'German'
+  },
+  {
+    key: 'Polish',
+    text: 'Polish',
+    value: 'Polish'
+  },
+  {
+    key: 'English',
+    text: 'English',
+    value: 'English'
+  }
+];
 
 function App() {
+  const [country, setCountry] = useState(undefined);
+  const [gender, setGender] = useState(undefined);
+  const [nationality, setNationality] = useState(undefined);
+  const [dateOfBirth, setDateOfBirth] = useState(undefined);
+  const [passExpirationDate, setPassExpirationDate] = useState(undefined);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Input placeholder="Name"/>
+      <Input placeholder="Surname"/>
+      <Input placeholder="eg. 234234"/>
+      <Dropdown value={country} selection options={countryOptions} onChange={(_, { value }) => setCountry(value)}/>
+      <Radio label="Male" checked={gender === "M"} onClick={() => setGender("M")}/>
+      <Radio label="Female" checked={gender === "F"} onClick={() => setGender("F")}/>
+      <Radio label="Not specified" checked={gender === "N"} onClick={() => setGender("N")}/>
+      <Dropdown value={nationality} selection options={nationalityOptions} onChange={(_, { value }) => setNationality(value)}/>
+      <DateInput value={dateOfBirth} onChange={(_, { value }) => setDateOfBirth(value)} placeholder="Date of Birth"/>
+      <DateInput value={passExpirationDate} onChange={(_, { value }) => setPassExpirationDate(value)} placeholder="Passport expiration date"/>
     </div>
   );
 }
